701d1b2b8f0a89436dfab6a322639122b98d1703
Shao-Hung Chan <shaohung@usc.edu>
2022-03-09 14:20:07 +0200

    Make PBS works for boost 1.71.0

---
+++ run/random-32-32-20/run_pbs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "Running Experiments: PBS"
+
+# name="random-32-32-20"
+name="den520d"
+map="/home/rdaneel/mapf_benchmark/mapf-map/$name.map"
+scen1="even"
+scen="/home/rdaneel/mapf_benchmark/scen-$scen1/$name-$scen1"
+output="/home/rdaneel/my_exp/$name/PBS0/$name-$scen1"
+
+for n in $(seq 200 200 800)
+do
+    for i in $(seq 1 1 25)
+    do
+        echo "$n agents on instance $name-$scen1-$i"
+        ../../build/PBS -m $map -a $scen-$i.scen -k $n -f false -o $output-$n-PBS0.csv
+    done
+done
