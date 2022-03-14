f38a27f0b82595127887faaeec672909cd17d2cc
Shao-Hung Chan <shaohung@usc.edu>
2022-03-14 10:36:01 +0200

    Modify sh file.

--- run/random-32-32-20/run_pbs.sh
+++ run/random-32-32-20/run_pbs.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 echo "Running Experiments: PBS"
 
-name="Berlin_1_256"
-# name="den520d"
+name="random-32-32-20"
 map="/home/rdaneel/mapf_benchmark/mapf-map/$name.map"
 scen1="even"
 scen="/home/rdaneel/mapf_benchmark/scen-$scen1/$name-$scen1"
-output="/home/rdaneel/my_exp/$name/PBS0/$name-$scen1"
+output="/home/rdaneel/my_exp/$name/PBS1_master/$name-$scen1"
 
-for n in $(seq 50 50 200)
+for n in $(seq 20 20 100)
 do
     for i in $(seq 1 1 25)
     do
         echo "$n agents on instance $name-$scen1-$i"
-        ../../build/PBS -m $map -a $scen-$i.scen -k $n -f false -o $output-$n-PBS0.csv
+        ../../build/PBS -m $map -a $scen-$i.scen -k $n -f false -o $output-$n-PBS1_master.csv
     done
 done
